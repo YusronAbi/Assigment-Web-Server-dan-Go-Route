0aaed779024b467c10bda13a71c3796583b4b55c
YusronAbi <yusronabimuliabakhtiar@gmail.com>
2024-11-23 19:25:25 +0700

    Initial commit

---
+++ Database/sample.sql
@@ -0,0 +1,17 @@
+INSERT INTO Produk (nama, deskripsi, harga, kategori)
+VALUES
+('Laptop', 'Laptop gaming dengan spesifikasi tinggi', 15000000.00, 'Elektronik'),
+('Kursi', 'Kursi ergonomis untuk kantor', 1200000.00, 'Furniture'),
+('Meja', 'Meja kayu solid untuk ruang kerja', 3000000.00, 'Furniture');
+
+INSERT INTO Inventaris (id_produk, jumlah, lokasi)
+VALUES
+(1, 10, 'Gudang A'),
+(2, 50, 'Gudang B'),
+(3, 20, 'Gudang A');
+
+INSERT INTO Pesanan (id_produk, jumlah)
+VALUES
+(1, 2),
+(2, 5),
+(3, 1);
